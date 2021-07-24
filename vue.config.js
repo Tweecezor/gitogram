7a47d5bfb9c07407c1c89b7461048c81e51cacc4
Tweecezor <thenamelessmanhd@gmail.com>
2021-07-24 19:26:58 +0300

    asg

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const vueSrc = "./src";
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/gitogram/" : "/",
   runtimeCompiler: true,
   css: {
     modules: true
